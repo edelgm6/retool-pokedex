ae75e64656737ad51118c9ac87f9de45ec2d8a73
Garrett <edelgm6@gmail.com>
2020-02-09 10:03:17 -0800

    Added local_settings and migrations

--- pokedex/settings.py
+++ pokedex/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^xcrhz*d3^nr$%4&mvt)+fm3#crdoprl9h7&e13uy&eg34$79w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from pokedex.local_settings import *
+except ImportError: #pragma: no coverage
+    pass
